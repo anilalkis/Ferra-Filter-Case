a4d9cf51df232929679c9b858e0067320702eb05
Anil Alkis <anilalkis89@gmail.com>
2025-04-18 17:25:25 +0300

    Add project files.

---
+++ FerraFilterCase.Entities/Ferra_orjinal_muadil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerraFilterCase.Entities
+{
+    public class Ferra_orjinal_muadil
+    {
+        public string ferra_no_b { get; set; }
+        public string firma_adi { get; set; }
+        public string filtre_no_b { get; set; }
+        public string filtre_no_goster { get; set; }
+        public string hangi_katalog { get; set; }
+        public string orjinal_muadil { get; set; }
+        public string sabit_degisken { get; set; }
+    }
+}
